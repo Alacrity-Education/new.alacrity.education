1ac4b0c14b99ad7405ca24b24cb3939f0b094273
Luca Sainenco <lucasainenco2@gmail.com>
2026-02-14 14:59:36 +0100

    add init migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260214_135919 from './20260214_135919';
+
+export const migrations = [
+  {
+    up: migration_20260214_135919.up,
+    down: migration_20260214_135919.down,
+    name: '20260214_135919'
+  },
+];
