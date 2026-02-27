2f87d20720fe16703c4cb62c03d69c517379fa2f
Luca Sainenco <lucasainenco2@gmail.com>
2026-02-28 00:22:23 +0100

    fix: expect error as there might be mismatch between types

--- src/blocks/RenderBlocks.tsx
+++ src/blocks/RenderBlocks.tsx
@@ -39,9 +39,10 @@ export const RenderBlocks: React.FC<{
             const Block = blockComponents[blockType]
 
             if (Block) {
+
               return (
                 <div className="my-16" key={index}>
-
+                  {/* @ts-expect-error there may be some mismatch between the expected types here */}
                   <Block {...block} disableInnerContainer />
                 </div>
               )
